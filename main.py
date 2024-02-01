96df169e610c35287ef3e695fb3a5d17e31b248f
JeanD <jean.duffy@hotmail.com>
2024-02-01 17:30:28 +0100

    End of day

--- main.py
+++ main.py
@@ -7,4 +7,21 @@ if __name__ == "__main__":
     #print('main 2')
 
     wikipediascraper.get_countries()
+
+    for country in wikipediascraper.countries_list:
+        wikipediascraper.get_leaders(country=country)
+    
+    '''
+    counter_index_date = 0
+    for url in wikipediascraper.wikipedia_url_list:
+
+        wikipediascraper.get_first_paragraph(url, wikipediascraper.birth_date_list[counter_index_date])
+        counter_index_date += 1
+    '''
+    for i in range(len(wikipediascraper.wikipedia_url_list)):
+
+        wikipediascraper.get_first_paragraph(wikipediascraper.wikipedia_url_list[i], wikipediascraper.birth_date_list[i])
+    print(len(wikipediascraper.wikipedia_url_list))
+
+
     #print('main 3')
--- src/scraper.py
+++ src/scraper.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 class WikipediaScraper:
 
@@ -7,7 +8,11 @@ class WikipediaScraper:
         self.country_endpoint = '/countries'
         self.leaders_endpoint = '/leaders'
         self.cookies_endpoint = '/cookie'
+        self.wikipedia_url_list = []
+        self.birth_date_list = []
+        self.leader_name_list = []
         self.leaders_data = {}
+        self.counter = 0
         self.cookie = None #cookie object
 
         
@@ -31,12 +36,52 @@ class WikipediaScraper:
 
     def get_leaders(self, country: str) -> None:
         '''Populates the leader_data object with the leaders of a country retrieved from the API'''
-        pass
+        self.country = country
+        #print(self.country)
+        self.req_leaders = requests.get(self.base_url + self.leaders_endpoint, params={'country' : country}, cookies=self.refresh_cookie().cookies).json()
+        
+        for leader in self.req_leaders:
+            self.wikipedia_url_list.append(leader.get('wikipedia_url'))
+
+            if leader.get('birth_date'):
+                birthdate = leader.get('birth_date').split('-')
+                self.birth_date_list.append(birthdate[0])
+            else:
+                self.birth_date_list.append('99999')
+
+            # Check this
+            if leader.get('last_name'):
+                self.leader_name = f'{leader.get('first_name')} {leader.get('last_name')}'
+            else:
+                self.leader_name = f'{leader.get('first_name')}'
+
     
 
-    def get_first_paragraph(self, wikipedia_url: str) -> str:
+    def get_first_paragraph(self, wikipedia_url: str, birth_year) -> str:
         '''Returns the first paragraph (defined by the HTML tag <p>) with details about the leader'''
-        pass
+        self.wikipedia_url = wikipedia_url
+        self.birth_year = birth_year
+        print(birth_year,wikipedia_url)
+
+        r = requests.get(f'{self.wikipedia_url}').text
+        soup = BeautifulSoup(r, 'html')
+        paragraphs = soup.find_all('p')
+        self.counter +=1
+        for paragraph in paragraphs:
+            if birth_year in paragraph:
+                print('birth_year')
+                print(paragraph)
+                break
+            elif paragraph is None:
+                continue
+
+            elif paragraph.find('b'):
+                print('bold', self.counter)
+                print(paragraph)
+                break
+            else:
+                pass
+        
 
 
     def to_json_file(self, filepath: str) -> None:
